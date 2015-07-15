c8d0e57b40e10a465ee9855404d74b16ecbbec92
Jai Rajput <jai.rajput@SYS-88.(none)>
2015-07-15 14:05:30 +0530

    make it better

--- db/schema.rb
+++ db/schema.rb
@@ -11,8 +11,8 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20150715062831) do
+
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -206,7 +206,7 @@ ActiveRecord::Schema.define(version: 20150715062831) do
     t.integer "user_id"
     t.integer "group_id"
   end
-  
+
   create_table "users_interests", id: false, force: :cascade do |t|
     t.integer "user_id"
     t.integer "interest_id"
