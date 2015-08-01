e40c0f1c77fbc6dec08b3784b8197d89e1b134dc
shekhar madnawat <shekhar.madnawat@mobiloittegroup.com>
2015-08-01 19:01:05 +0530

    commit after common interest algo

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -8,17 +8,19 @@ class Interest < ActiveRecord::Base
   has_and_belongs_to_many :users , :join_table => "users_interests" 
 
 
-  def self.view_matches_algo selected_interest
+  def self.view_matches_algo selected_interest, user
   	matches = []
-  	selected_interest.each do |interest|
-  		matches << interest.users
+  	final = []
+	  	selected_interest.each do |interest|
+	  		interest.users.where('id != ?', user.id).each do |match|
+	  			matches << match if match.current_city == user.current_city
+	  		end
+	  	end
+  	matches.uniq.each do |t|
+  		final << t.profile
+  		final << t.interests&user.interests
   	end
-  	if matches.present?
-  		true
-  		matches.uniq
-  	else
-  		[]
-  	end
+  	final
   end
 
 end
