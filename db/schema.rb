ed5ff40d8f73cbbd03cde93dd6e40176abfb18f3
Jai Rajput <you@example.com>
2015-08-06 10:57:28 +0530

    add migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804134000) do
+ActiveRecord::Schema.define(version: 20150806050429) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -48,6 +48,16 @@ ActiveRecord::Schema.define(version: 20150804134000) do
 
   add_index "admin_users", ["email"], name: "index_admin_users_on_email", unique: true, using: :btree
   add_index "admin_users", ["reset_password_token"], name: "index_admin_users_on_reset_password_token", unique: true, using: :btree
+
+  create_table "albums", force: :cascade do |t|
+    t.string   "image"
+    t.boolean  "status"
+    t.integer  "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "albums", ["user_id"], name: "index_albums_on_user_id", using: :btree
 
   create_table "categories", force: :cascade do |t|
     t.string   "category_name"
@@ -262,6 +272,7 @@ ActiveRecord::Schema.define(version: 20150804134000) do
     t.integer "interest_id"
   end
 
+  add_foreign_key "albums", "users"
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "feedbacks", "users"
