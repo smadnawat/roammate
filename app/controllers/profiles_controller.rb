79e136ffe8e337153534c671a427b55002b291ad
Jai Rajput <jai.rajput@mobiloittegroup.com>
2015-08-10 19:14:02 +0530

    commit

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -14,10 +14,18 @@ class ProfilesController < ApplicationController
 	def view_matched_profile
 		@member = User.find_by_id(params[:member_id])
 		if @member.present?
+		
 			@interests = common_activities(@user.id, @member.id)
 			@points = point_algo(@user.id,@member.id)
 			@mutual_friends = common_friends(@user.id, @member.id)
 			@common_friends = Profile.where('id IN (?)', @mutual_friends)
+			@group = Group.where("(group_admin = ?  and group_name = ?) or (group_admin = ?  and group_name = ?)",@user.id,@member.id.to_s,@member.id,@user.id.to_s ).first
+			@ratings = nil
+			@ratable = false
+			if @group.present?
+				@ratings = @member.ratings.where(rater_id: @user.id).first.rate
+				@ratable = message_count(@user,@group)
+			end
 			render :json => {
 											:response_code => 200, :message => "record successfully fetched",
 											:member_profile => @member.profile,
@@ -25,7 +33,9 @@ class ProfilesController < ApplicationController
 											:mutual_interests_count => @interests.count,
 											:mutual_friends => @common_friends,
 											:mutual_friends_count => @mutual_friends.count,
-											:points => @points
+											:points => @points,
+											:rate => @ratings,
+											:can_rate => @ratable,
 											}
 		else
 			render :json => {
--- app/uploaders/file_uploader_uploader.rb
+++ app/uploaders/file_uploader_uploader.rb
@@ -6,6 +6,7 @@ class FileUploaderUploader < CarrierWave::Uploader::Base
   def extension_white_list
     %w(csv)
   end
+  
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
