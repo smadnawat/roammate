d1ad1663d2f70be9dd72029fdc15d550e8b96075
shekhar madnawat <shekhar.madnawat@mobiloittegroup.com>
2015-09-25 15:07:33 +0530

    change and add REDISTOGO_URL

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,7 +3,7 @@ Rails.application.configure do
 
   # Code is not reloaded between requests.
   config.cache_classes = true
-  REDISTOGO_URL: redis://redistogo:31353ba176e746bad3bb123732ccf0ec@tarpon.redistogo.com:11249/
+  REDISTOGO_URL: "redis://redistogo:31353ba176e746bad3bb123732ccf0ec@tarpon.redistogo.com:11249/"
   # Eager load code on boot. This eager loads most of Rails and
   # your application in memory, allowing both threaded web servers
   # and those relying on copy on write to perform better.
