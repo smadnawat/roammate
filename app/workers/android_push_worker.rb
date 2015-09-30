8ad16cd8be556753f1d821ecdb44aee082e3065a
shekhar madnawat <shekhar.madnawat@mobiloittegroup.com>
2015-09-30 21:38:16 +0530

    push workers

--- app/workers/android_push_worker.rb
+++ app/workers/android_push_worker.rb
@@ -3,7 +3,7 @@ class AndroidPushWorker
   include Sidekiq::Worker
 
     def perform(reciever,alert,badges,notification,invitation,type, device_id, image, name, group_id)
-      logger.info"=======image=#{image}========group_id==#{group_id}====notification_id=#{notification}=======group_name=#{name}========reciever_id==#{reciever.inspect}=======alert=#{alert.inspect}=========badges=#{badges.inspect}=============device_id=#{device_id.inspect}=========notification_type=#{type.inspect}==========invitation_id==#{invitation.inspect}======="
+      p "=======image=#{image}========group_id==#{group_id}====notification_id=#{notification}=======group_name=#{name}========reciever_id==#{reciever.inspect}=======alert=#{alert.inspect}=========badges=#{badges.inspect}=============device_id=#{device_id.inspect}=========notification_type=#{type.inspect}==========invitation_id==#{invitation.inspect}======="
          p"==============#{device_id}=================IN ANDROID WORKER"
         gcm = GCM.new("AIzaSyDGIkZOsPJIqf6p6m9dWuZq_bFtEvMNcnM")
         registration_ids = ["#{device_id}"]         
@@ -16,7 +16,7 @@ class AndroidPushWorker
             "collapse_key" => 'updated_state'
             }
         response = gcm.send_notification(registration_ids,options)
-        puts "=============Done======#{response.inspect}======== ANDROID WORKER"
+        p "=============Done======#{response.inspect}======== ANDROID WORKER"
       end
 
 end
--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,10 +6,10 @@ before_fork do |server, worker|
   # Replace with MongoDB or whatever
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.connection.disconnect!
-    Rails.logger.info('Disconnected from ActiveRecord')
+    p "('Disconnected from ActiveRecord')"
   end
 
-  Rails.logger.info('Disconnected from Redis')
+  p "('Disconnected from Redis')"
 
   sleep 1
 end
@@ -18,12 +18,12 @@ after_fork do |server, worker|
   # Replace with MongoDB or whatever
   if defined?(ActiveRecord::Base)
     ActiveRecord::Base.establish_connection
-    Rails.logger.info('Connected to ActiveRecord')
+    p "('Connected to ActiveRecord')"
   end
 
   Sidekiq.configure_client do |config|
     config.redis = { url: ENV["REDISTOGO_URL"] }
   end unless ENV["REDISTOGO_URL"].blank?
 
-  Rails.logger.info('Connected to Redis')
+  p "('Connected to Redis')"
 end
