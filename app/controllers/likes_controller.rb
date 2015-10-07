961f39b395f404507c1e7f7c6ce369b89729c2aa
shekhar madnawat <shekhar.madnawat@mobiloittegroup.com>
2015-10-07 12:33:53 +0530

    make it better

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -3,46 +3,43 @@ class LikesController < ApplicationController
 	before_filter :check_user
 	def like_event
 		@event = Event.find_by_id(params[:event_id])
-			if @user && @event
-					if @like = Like.find_by(user_id: @user.id, event_id: @event.id).present?
-
-							if @like = Like.find_by(user_id: @user.id, event_id: @event.id, status: false)
+			if (@user && @event)
+					@count = @event.likes.where(status: true).count
+					if @like = Like.find_by(user_id: @user.id, event_id: @event.id)
+							if (@like.status == false)
 								@like.update_attributes(:status => true)
-								@count = @event.likes.where(status: true).count
-								render :json => {
-																:response_code => 200,
-																:status => @like.status,
-																:count => @count,
-																:message => "LIKE."
-																}
+								code = 200
+								message = "Like created successfully"
 							else	
-								@like = Like.find_by(user_id: @user.id, event_id: @event.id, status: true)		
 								@like.update_attributes(:status => false)
-								@count = @event.likes.where(status: true).count
-								render :json => {
-																:response_code => 200,
-																:status => @like.status,
-																:count => @count,
-																:message => "UNLIKE ."
-																}
+								code = 200
+								message = "UnLike created successfully"
 							end
-					else
-							@like = @user.likes.build(:user_id => params[:user_id], :event_id => params[:event_id], :status => params[:status] )
-							@like.save
-							@count = @event.likes.where(status: true).count
 							render :json => {
-															:response_code => 200,
+															:response_code => code,
 															:status => @like.status,
-															:count => @count,
-															:message => "Like created."
+															:count => Like.where("event_id =? and status =?", @event.id, true).count,
+															:message => message
+															}
+					else
+							@like = @user.likes.build(:user_id => params[:user_id], :event_id => params[:event_id], :status => true )
+							@like.save
+							code = 200
+							message = "Like created successfully"
+							render :json => {
+															:response_code => code,
+															:status => @like.status,
+															:count => Like.where("event_id =? and status =?", @event.id, true).count,
+															:message => message
 															}
 					end
 			else
 				render :json => {
 												:response_code => 500,
-												:message => "Something went wrong. "
+												:message => "Something went wrong"
 												}
 			end
 	end
 
 end
+
